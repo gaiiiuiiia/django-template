7dc73845e10cbdbd1312a11a77a0d17ef811320f
maksim.titov <gaiiiuiiialikeaboss@gmail.com>
2022-10-08 20:57:57 +0300

    change static and media folder to the parent directory, add secrets template

---
+++ app/settings/secrets_template.py
@@ -0,0 +1,13 @@
+import os
+
+secrets = {
+    'DJANGO_SECRET_KEY':            'django-secret-key',
+
+    'POSTGRES_DB':                  'database-name',
+    'POSTGRES_USER':                'database-user',
+    'POSTGRES_PASSWORD':            'database-pass',
+    'POSTGRES_HOST':                'database-host',
+
+    'STATIC_FILES_FOLDER':          'static',
+    'MEDIA_FILES_FOLDER':           'media',
+}
